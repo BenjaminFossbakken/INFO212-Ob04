eab461bd9fbc90d9affe5da592e8c2c58f12b32a
BenjaminFossbakken <benjamin.fossbakken@gmail.com>
2024-10-28 14:44:29 +0100

    Changed display to 'READ' and updated 'CREATE' to have basic info

--- api.py
+++ api.py
@@ -14,27 +14,33 @@ print(username, pwd, uri)
 driver=GraphDatabase.driver(uri=uri,auth=(username,pwd))
 session=driver.session()
 
+#CREATE
 api=Flask(__name__)
-@api.route("/create/<string:name>&<int:id>",methods=["GET", "POST"])
-def create_node(name, id):
+@api.route("/create/<string:name>&<int:id>&<string:address>&<string:branch>",methods=["GET", "POST"])
+def create_node(name, id, address, branch):
     q1="""
-    create (n:Employee{NAME:$name, ID:$id})
+    CREATE (e:Employee{NAME:$name, ID:$id, ADDRESS: $address, BRANCH: $branch})
     """
-    map={"name": name, "id": id}
+    map={"name": name, "id": id, "address": address, "branch": branch}
     try:
         session.run(q1, parameters=map)
-        return (f"employee node is created with employee name={name} and id={id}")
+        return (f"Employee node is created with employee name={name} and id={id}, Address={address}, Branch={branch}")
     except Exception as e:
         return (str(e))
 
-@api.route("/display", methods=["GET", "POST"])
+#READ
+@api.route("/employees", methods=["GET"])
 def display_node():
     q1="""
-    match (n) return n.NAME as NAME ,n.ID as ID
+    MATCH (e:Employee) RETURN e.NAME AS name, e.ID AS id, e.ADDRESS AS address, e.BRANCH AS branch
     """
     results=session.run(q1)
     data=results.data()
     return(jsonify(data))
+
+#UPDATE
+
+#DELETE
 
 if __name__=="__main__":
     api.run(port=5050)
